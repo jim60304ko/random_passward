405c513fce0f5e5fc1fb86891bc2ee4759828223
jim60304ko <jim60304ko@gmail.com>
2020-03-26 21:48:54 +0800

    Add annotations

--- main.py
+++ main.py
@@ -2,13 +2,12 @@
 """
 Created on Tue Mar 24 21:08:39 2020
 
-@author: jim60
+@author: C.-C. Wang
 """
+# import all needed package
 from tkinter import Tk, Label, Button, StringVar
 from random import choice
-#%%
-#random.seed(10)
-#%% build the ascii code list
+# build the ascii code list
 random_list = []
 def build_random_list(a, b, lst):
     for i in range(a, b+1):
@@ -17,24 +16,22 @@ def build_random_list(a, b, lst):
 random_list = build_random_list(48, 57, random_list)
 random_list = build_random_list(65, 90, random_list)
 random_list = build_random_list(97, 122, random_list)
-#%%
+# Create the random passward (default size = 12)
 def create_random_passward(length=12):
     lst = ''
     for i in range(length):
         lst += chr(choice(random_list))
     return lst
 passward = create_random_passward()    
-#%%
-#print(passward)
-#%%
+# The width and height of the GUI
 width = 300
 height = 75
-#%% The environmnet setting
+# The environmnet setting
 window = Tk()
 window.title('Random Passward')
 window.geometry(str(width)+"x"+str(height)+"+480+270")
 window.resizable(False, False)
-#%%
+# The label that show the passward
 var = StringVar()
 show_label = Label(window,
             textvariable=var, 
@@ -42,14 +39,14 @@ show_label = Label(window,
             font=('Arial',12), 
             width=50, height=1)
 show_label.pack()
-#%%
+# The instruction label
 text_label = Label(window,
             text='Passward Length',
             #bg='white',
             font=('Arial',12),
             width=30, height=1)
 text_label.pack()
-#%%
+# The button that create the passwards in length of 8, 10, 15, 20
 def show_passward_8():
     var.set(create_random_passward(8))
 show_passward_button_8 = Button(window, text='8', width=8, height=1, command=show_passward_8)
@@ -66,5 +63,5 @@ def show_passward_20():
     var.set(create_random_passward(20))
 show_passward_button_20 = Button(window, text='20', width=8, height=1, command=show_passward_20)
 show_passward_button_20.place(x=width*0.75+5,y=48,anchor='nw')
-    #%% Run the program
+# Run the program
 window.mainloop()
